882b2358e101831f098d40ec405628fc295a2e80
Ilya Shulepov <24671528+KernelMrex@users.noreply.github.com>
2026-04-23 19:00:13 +0300

    feat: added application/octet-stream content type

--- pkg/httpx/content_type.go
+++ pkg/httpx/content_type.go
@@ -1,6 +1,7 @@
 package httpx
 
 const (
-	ApplicationJSON     = "application/json"
-	ApplicationJSONUTF8 = "application/json; charset=utf-8"
+	ApplicationJSON        = "application/json"
+	ApplicationJSONUTF8    = "application/json; charset=utf-8"
+	ApplicationOctetStream = "application/octet-stream"
 )
